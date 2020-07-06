c907dbf129985e62b0cc0e4abee9db296cbb5cd8
Dorgival Fernando <59142443+Dorgival-lab@users.noreply.github.com>
2020-07-06 07:20:55 -0300

    Create to print all Prime numbers in an Interval.py
    Um número primo é um número natural maior que 1 que não possui divisores positivos além de 1 e ele próprio. Os primeiros números primos são {2, 3, 5, 7, 11,….}

---
+++ to print all Prime numbers in an Interval.py
@@ -0,0 +1,14 @@
+# Programa Python para imprimir tudo
+# número primo em um intervalo 
+  
+start = 11
+end = 25
+  
+for val in range(start, end + 1): 
+    if val > 1: 
+        for n in range(2, val//2 + 2): 
+            if (val % n) == 0: 
+                break
+            else: 
+                if n == val//2 + 1: 
+                    print(val) 
